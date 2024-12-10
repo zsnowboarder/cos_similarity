f0a9819fdd6b45f5d68808536e8aa4be8259cea5
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2024-12-09 21:18:56 -0800

    Add files via upload

--- sent_transformer.py
+++ sent_transformer.py
@@ -83,6 +83,8 @@ if st.button(label="Search"):
     df['Similarity Score'] = df['Descriptions'].apply(calculate_similarity)
     st.table(df)
 
+st.write("This demo only shows an example of identifyimg a similar vehicle. It can be MO or anything and the possibilities are endless.")
+
 
 # In[ ]:
 
